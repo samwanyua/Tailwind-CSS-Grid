fa321a383fb4156730bb21e59800cf039e7734a9
samwanyua <samexample8@gmail.com>
2024-02-20 19:46:35 +0300

    Layout 1

--- src/components/Catalogue.js
+++ src/components/Catalogue.js
@@ -2,9 +2,9 @@ import {data} from "../data"
 const Catalogue = () => {
     const boxStyle = `bg-neutral-100 p-2 border-2 border-gray-300 flex flex-col justify-center items-center`
   return (
-    <div className="grid grid-cols-3 auto-rows-[300px] gap-4 my-10">
+    <div className="grid md:grid-cols-3 auto-rows-[300px] gap-4 my-10">
         {data.map((item, index) => (
-            <div key={index} className={`${boxStyle}`}>
+            <div key={index} className={`${boxStyle} ${index === 3 || index ===6 ? 'md:col-span-2 bg-black text-white' : ''}`}>
                 <h2 className="font-light text-[12px] uppercase">{item.title}</h2>
                 <p className="font-black text-2xl">{item.value}</p>
             </div>
