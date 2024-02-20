66fa6e0f0170be3aa1084478209ce3516c005d01
samwanyua <samexample8@gmail.com>
2024-02-20 19:58:11 +0300

    Responsive Layout 2

--- src/components/Catalogue.js
+++ src/components/Catalogue.js
@@ -2,9 +2,13 @@ import {data} from "../data"
 const Catalogue = () => {
     const boxStyle = `bg-neutral-100 p-2 border-2 border-gray-300 flex flex-col justify-center items-center`
   return (
-    <div className="grid grid-cols-3 auto-rows-[300px] gap-4 my-10">
+    <div className="grid md:grid-cols-4 auto-rows-[300px] gap-4 my-10">
         {data.map((item, index) => (
-            <div key={index} className={`${boxStyle}`}>
+            <div key={index} className={`${boxStyle} 
+                ${index === 0 || index === 3 ? 'bg-black md:col-span-2 text-white' : ''}
+                ${ index === 5 || index === 6 ? 'bg-black md:col-span-3 text-white' : ''}
+                ${index === 2 ? 'md:row-span-4 bg-black text-white' : ''}
+            `}>
                 <h2 className="font-light text-[12px] uppercase">{item.title}</h2>
                 <p className="font-black text-2xl">{item.value}</p>
             </div>
